d9ba3d45129eca52b62088da7df4d53421ef3149
Johannes <johannes.zierenberg@gmail.com>
2020-01-16 09:22:55 +0100

    added probabilistic integrate and fire model (pif)

--- DR_pif_linear.cpp
+++ DR_pif_linear.cpp
@@ -1,6 +1,4 @@
-// Branching network model with Poissonian external input of mean rate h
-// standard branching parameter m=const
-// binomial selection reproduces fully-connected network
+// Probabilisitic integrate-and-fire model with Poissonian external input of mean rate h
 #include <iostream>
 #include <fstream>
 #include <sstream>
@@ -22,6 +20,7 @@ int main(int argc, char* argv[]){
   double   T = 1e0;  // time steps 
   double   h = 1.00; // external drive
   double   m = 0;    // synaptic strength
+  double   p_edge = 0.01; // probability of edge in ER network   
   unsigned seed=1000;
 
   std::stringstream help;
@@ -30,6 +29,7 @@ int main(int argc, char* argv[]){
   help << "     -T : number of time steps  (T=1e7 ms goal )\n";
   help << "     -h : external input (h<1)\n";
   help << "     -m : synaptic strength\n";
+  help << "     -p : edge probability of random graph\n";
   help << "     -s : seed\n";
   int valid_args=0;
   for(unsigned i=0; i<argc; i++){
@@ -38,8 +38,9 @@ int main(int argc, char* argv[]){
     if(std::string(argv[i])=="-h"){ h    = atof(argv[i+1]); valid_args+=1;}
     if(std::string(argv[i])=="-m"){ m    = atof(argv[i+1]); valid_args+=1;}
     if(std::string(argv[i])=="-s"){ seed = atof(argv[i+1]); valid_args+=1;}
+    if(std::string(argv[i])=="-p"){ p_edge = atof(argv[i+1]); valid_args+=1;}
   }
-  if(valid_args<5){std::cout << "not enough arguments\n" << help.str() << std::endl; exit(1);}
+  if(valid_args<6){std::cout << "not enough arguments\n" << help.str() << std::endl; exit(1);}
   
   std::vector<int> neuron(N,0);
   std::vector<int> stimulus(N,0);
@@ -48,11 +49,27 @@ int main(int argc, char* argv[]){
   std::mt19937                           mt(seed);   
   std::uniform_real_distribution<double> uni_01(0,1);  // r=uni_01(mt)
 
-  //initialize the binomial distributions from which the neurons-to-be-activated are drawn
-  double p_std = m/static_cast<double>(N);
-  std::binomial_distribution<int> binom(N,p_std);
+  //generate_ER
+  std::vector<std::vector<unsigned>> outgoing(N, std::vector<unsigned>());
+  std::vector<std::vector<unsigned>> incoming(N, std::vector<unsigned>());
+  for(unsigned n=0;n<neuron.size(); n++){
+    for(unsigned nn=0;nn<neuron.size(); nn++){
+      if(n!=nn){
+        if(uni_01(mt)<p_edge){
+          outgoing[n ].push_back(nn);
+          incoming[nn].push_back(n );
+        }
+      }
+    }
+  }
+
+  //linear activation function:
+  std::vector<double> w(N,0);
+  for(unsigned n=0; n<w.size(); n++) w[n] = m/incoming[n].size();
 
   //loop over time
+  long unsigned T_therm = int(1.0/lambda(h));
+  std::cout << "#T_therm="<<T_therm <<"\n";
   std::cout << "#simulation\n";
 
   //observable
@@ -60,28 +77,23 @@ int main(int argc, char* argv[]){
   double avg_activity  = 0;
   double avg_activity2 = 0;
   //loop over avalanche onsets
-  int num_active=0, num_active_int=0;
-  while(time < T){
+  int num_active=0, num_active_old=0;
+  while(time < T+T_therm){
+    num_active_old = num_active;
     // internal signal processing (branching process)
     for(unsigned n=0;n<neuron.size(); n++){
-      //annealed average: select random postsynaptic neurons for each active neuron 
-      if(neuron[n] > 0 ){
-        int k_n = binom(mt) ;
-        std::list<double> nconnect;
-        int j=0;
-        while(j<k_n){
-          int nn = uni_01(mt)*neuron.size();
-          if(std::find(nconnect.begin(), nconnect.end(), nn) == nconnect.end()){
-            nconnect.push_back(nn);
-            stimulus[nn] += 1;
-            j+=1;
-          }
-        }
-      }
       //random external activation with probability h
       if(uni_01(mt) < lambda(h)) { 
         stimulus[n] = 1; 
       } 
+      // internal probabilistic integrate and fire mechanism
+      else{
+        int num_active_neigbors = 0;
+        for(unsigned i=0; i< incoming[n].size(); i++) num_active_neigbors += neuron[incoming[n][i]];
+        if(uni_01(mt) < w[n]*num_active_neigbors) { 
+          stimulus[n] = 1; 
+        }
+      }
     }
     // integration over internal activation and external activation
     // num_active remains the number of initially active neurons that lead to this activation
@@ -92,18 +104,22 @@ int main(int argc, char* argv[]){
          num_active +=1;
          stimulus[n]=0;
       }
-      else if(neuron[n]==1){ neuron[n]=0; }
+      else if(neuron[n]==1){ 
+        neuron[n]=0; 
+      }
     }
     time++;
-    avg_activity  += num_active;
-    avg_activity2 += num_active*num_active;
+    if(time > T_therm){
+      avg_activity  += num_active;
+      avg_activity2 += num_active*num_active;
+    }
   }
-  avg_activity /= static_cast<double>(time);
-  avg_activity2/= static_cast<double>(time);
-  double err_activity  = sqrt((avg_activity2 - avg_activity*avg_activity)/(static_cast<double>(time)-1));
+  avg_activity /= static_cast<double>(T);
+  avg_activity2/= static_cast<double>(T);
+  double err_activity  = sqrt((avg_activity2 - avg_activity*avg_activity)/(static_cast<double>(T)-1));
   std::cout << "#seed statistic  avg_activity  avg_activity2  error(activity)\n";
   std::cout << seed << " ";
-  std::cout << time << " ";
+  std::cout << T << " ";
   std::cout << avg_activity  << " ";
   std::cout << avg_activity2 << " ";
   std::cout << err_activity  << " ";
--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@
 CC=g++ -std=c++11 -O2
 
 all: corrected standard 
-corrected: DR_BN_fsc 
+corrected: DR_BN_fsc DR_pif_linear 
 standard: DR_BN_std
 
 DR_BN_fsc: DR_BN_fsc.cpp
@@ -10,3 +10,7 @@ DR_BN_fsc: DR_BN_fsc.cpp
 
 DR_BN_std: DR_BN_std.cpp
 	$(CC) -o DR_BN_std DR_BN_std.cpp
+
+DR_pif_linear: DR_pif_linear.cpp
+	$(CC) -o DR_pif_linear DR_pif_linear.cpp
+
